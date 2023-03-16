73fa813c9a698fa1cc9fcbfcef9975908061fd89
mhamza1999 <muhammad.hamza@orio.tech>
2023-03-16 17:23:06 +0500

    logout work

--- lib/Widgets/AppBar/app_bar.dart
+++ lib/Widgets/AppBar/app_bar.dart
@@ -5,6 +5,9 @@ import 'package:orio_tech_attendance_app/Utils/Colors/color_resource.dart';
 import '../../Utils/Constant/text_context.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
+
+import '../Button/button.dart';
+import '../Button/outline_button.dart';
 class BuildAppBar extends StatelessWidget with PreferredSizeWidget {
   const BuildAppBar({
     Key? key,
@@ -63,7 +66,83 @@ class BuildAppBar extends StatelessWidget with PreferredSizeWidget {
                 focusColor: Colors.transparent,
                 hoverColor: Colors.transparent,
                 highlightColor: Colors.transparent,
-                onTap: onLogout,
+                onTap: ()=> showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: SizedBox(
+                        height: 280,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 245, 99, 9)
+                                    .withOpacity(0.3),
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Icon(
+                                Icons.question_mark,
+                                size: 60,
+                                color: Colors.red,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Text(
+                              'Alert',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 25.0,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            const Text(
+                              'Are you sure you want to Logout?',
+                              style: TextStyle(
+                                color: Color(0xFF6E6E6E),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: OuluttlineButton(
+                                      child: const Text("cancel"),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      }),
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: Button(
+                                      onPressed: ()async{
+                                        await GetStorage.init();
+                                        await GetStorage().erase();
+                                        Get.offAllNamed(LoginScreen.routeName);
+                                      },
+                                      child: const Text("Logout")),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
                 child: SvgPicture.asset(
                   'assets/icons/logout.svg',
                 ),
